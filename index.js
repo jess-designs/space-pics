de24cd0735cb40ec00fdfcb20b3408479f86e83d
Jess <jessica.annd760@gmail.com>
2023-11-13 17:34:45 -0800

    added procfile and updated port to add process.env.port

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ dotenv.config();
 
 const apiKey = process.env.NASA_API_KEY;
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 
 app.use(express.static("public"));
